eaa173cf8aa36350ee241be67fb48fa02d762aac
Vina Compson <vv.compson@icloud.com>
2016-12-20 01:40:04 +1100

    initial and final commit' ;

---
+++ controllers/drivers_controller.js
@@ -0,0 +1,43 @@
+const Driver = require('../models/driver');
+
+module.exports = {
+  greeting(req,res){
+    res.send({hi: 'there'});
+  },
+
+  index(req, res, next){
+    const { lng, lat } = req.query;
+    Driver.geoNear(
+      { type: 'Point', coordinates: [ parseFloat(lng), parseFloat(lat)] },
+      { spherical: true, maxDistance: 200000 }
+    )
+      .then(drivers => res.send(drivers))
+      .catch(next);
+  },
+
+  create(req, res, next){
+    const driverProps = req.body;
+
+    Driver.create(driverProps)
+      .then(driver => res.send(driver))
+      .catch(next);
+  },
+
+  edit(req, res, next){
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then((driver) => Driver.findByUd( { _id: driverId }))
+      .then(driver => res.send(driver))
+      .catch(next);
+  },
+
+  delete(req, res, next) {
+    const driverId = req.params.id;
+
+    Driver.findByIdAndRemove({ _id: driverId })
+      .then(driver => resstatus(204).send(driver))
+      .catch(next);
+  }
+};
